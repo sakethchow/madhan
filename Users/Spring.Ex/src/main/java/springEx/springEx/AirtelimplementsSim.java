37aa4837241bb6e0a63532d12de33d9d979ab5bb
sakethchow <sakethchow33113@gmail.com>
2022-06-06 12:44:00 +0530

    madhan

---
+++ Users/Spring.Ex/src/main/java/springEx/springEx/AirtelimplementsSim.java
@@ -0,0 +1,30 @@
+package springEx.springEx;
+
+public class AirtelimplementsSim {
+
+	private String datastrength;
+	
+	public String getDatastrength() {
+		return datastrength;
+	}
+
+	public void setDatastrength(String datastrength) {
+		this.datastrength = datastrength;
+	}
+
+	public void typeOfSim() {
+		
+		System.out.println("this is airtel sim");
+	}
+
+	public void dataTypeOfSim() {
+		
+		System.out.println("airtel has 5G data");
+	}
+	
+	public void dataStrength() {
+		System.out.println(datastrength);
+	}
+
+}
+
